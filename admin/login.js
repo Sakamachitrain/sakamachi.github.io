3ff61cb5b251a482882936e7d655016fdff7dbcd
Sakamachitrain <sakamachitrain@gmail.com>
2025-11-21 18:18:07 +0900

    Update login.js

--- admin/login.js
+++ admin/login.js
@@ -1,4 +1,5 @@
-// SHA-256 文字列生成
+const PASSWORD_HASH = "2BC96DAAE784525EB66B7BC1132F14989E28BC3FC730526A46E08EA465194018";
+
 async function sha256(str) {
   const buf = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(str));
   return Array.from(new Uint8Array(buf))
@@ -6,17 +7,16 @@ async function sha256(str) {
               .join("");
 }
 
-// ★パスワード（ハッシュ保存）
-const PASSWORD_HASH = "2BC96DAAE784525EB66B7BC1132F14989E28BC3FC730526A46E08EA465194018";
-
 document.getElementById("btn").onclick = async () => {
-  const input = document.getElementById("pw").value;
+  const input = document.getElementById("pw").value.trim(); // ←trim()で前後空白を削除
   const hash = await sha256(input);
 
+  console.log("入力:", input);
+  console.log("ハッシュ:", hash);
+
   if (hash === PASSWORD_HASH) {
-    // ログイン状態を localStorage に保存
     localStorage.setItem("komashin_admin_login", "OK");
-    location.href = "index.html"; // 管理トップへ
+    location.href = "index.html";
   } else {
     document.getElementById("msg").textContent = "パスワードが違います。";
   }
